ceac9b94475ff71cc133b4034ae51ded361d45a7
idc9 <idc9@cornell.edu>
2017-06-15 20:37:58 -0400

    test viz

--- jive/tests/test_jive_visualization.py
+++ jive/tests/test_jive_visualization.py
@@ -23,22 +23,28 @@ class JiveViz(unittest.TestCase):
         self.X_obs, self.X_joint, self.X_indiv, self.X_noise, \
         self.Y_obs, self.Y_joint, self.Y_indiv, self. Y_noise = generate_data_ajive_fig2(seed)
 
-        blocks = [self.X_obs, self.Y_obs]
+        self.blocks = [self.X_obs, self.Y_obs]
         wedin_bound = True
         show_scree_plot = False
         full = True
 
         # compute JIVE decomposition
-        self.jive = Jive(blocks, wedin_bound, full, show_scree_plot)
+        self.jive = Jive(self.blocks , wedin_bound, full, show_scree_plot)
         self.jive.set_signal_ranks([2, 3])  # we know the true ranks
+
+    def test_block_plot(self):
+        """
+        Make sure plot_data_blocks() runs without error.
+        """
+        plot_data_blocks(self.blocks)
+        self.assertTrue(True)
 
     def test_jive_plot(self):
         """
-        Make sure the jive plot function runs without error.
+        Make sure plot_jive_full_estimates() runs without error.
         """
         plot_jive_full_estimates(self.jive)
         self.assertTrue(True)
-
 
 if __name__ == '__main__':
     unittest.main()
