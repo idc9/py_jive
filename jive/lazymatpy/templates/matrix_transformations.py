3a943a777a1216e23c585fbf2b993244a82a9943
idc9 <idc9@cornell.edu>
2017-10-24 13:09:31 -0400

    svd residuals

--- jive/lazymatpy/templates/matrix_transformations.py
+++ jive/lazymatpy/templates/matrix_transformations.py
@@ -17,3 +17,12 @@ def col_proj(M, U):
 def col_proj_orthog(M, U):
     """ (I - UU^T)M """
     return  (IdentityOperator(shape=(M.shape[0], M.shape[0]), dtype=M.dtype) - aslinearoperator(U) * aslinearoperator(U.T)) * aslinearoperator(M)
+
+
+def svd_residual(X, U, D, Vt):
+    """ X - UDVt """
+    Xlo = aslinearoperator(X)
+    Ulo = aslinearoperator(U)
+    DVtlo = aslinearoperator(dia_matrix(np.diag(D)).dot(Vt))
+
+    return Xlo - Ulo.dot(DVtlo)
