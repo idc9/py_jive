eda2bcd95e98d70eb00c39c86e0f54e2dcb8aeb1
idc9 <idc9@cornell.edu>
2018-11-05 12:34:03 -0500

    v0.1.2 prep

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/idc9/py_jive'
 EMAIL = 'idc9@cornell.edu'
 AUTHOR = 'Iain Carmichael'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
